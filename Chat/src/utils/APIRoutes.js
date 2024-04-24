649feeadb274711bc506969a26ac7b04260997e0
XCODE89 <maph0989@gmail.com>
2024-04-24 17:18:47 -0400

    mejoramiento de estilo

--- Chat/src/utils/APIRoutes.js
+++ Chat/src/utils/APIRoutes.js
@@ -1,4 +1,6 @@
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://chat-2ibo.onrender.com/";
+
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
